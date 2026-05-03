afd278a2434ef14e59d662a8274c2e5d3940433a
Apollo99-Dev <yashupadhye786@gmail.com>
2026-05-03 15:32:20 +0530

    Refactored controller to use service layer

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using CrudDemoApi.Data;
+using CrudDemoApi.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
+
+//Register Service
+builder.Services.AddScoped<IProductService, ProductService>();
 
 // Register In-Memory Database
 builder.Services.AddDbContext<AppDbContext>(options =>
---
+++ Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using CrudDemoApi.Models;
+
+namespace CrudDemoApi.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetAll(int pageNumber, int pageSize);
+        Product GetById(int id);
+        Product Create(Product product);
+        Product Update(int id, Product product);
+        bool Delete(int id);
+    }
+}
